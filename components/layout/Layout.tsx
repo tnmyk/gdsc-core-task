b231909409e761e0e549f458f785e50edc5ad417
Tanmaykk <tanmaykachroo@gmail.com>
2022-08-04 19:12:50 +0530

    Layout added

---
+++ components/layout/Layout.tsx
@@ -0,0 +1,10 @@
+import { ReactNode } from "react";
+
+const Layout = ({ children }: { children: ReactNode }) => {
+  return (
+    <>
+      <main>{children}</main>
+    </>
+  );
+};
+export default Layout;

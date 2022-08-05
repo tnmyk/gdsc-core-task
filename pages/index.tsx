71031ad2b1494d76df48093e5c7c64d47e0460f1
Tanmaykk <tanmaykachroo@gmail.com>
2022-08-05 20:32:22 +0530

    Search Nav

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
+import { CgOptions } from "react-icons/cg";
 import IconedInput from "../components/IconedInput";
-
+import { BiBookmarks } from "react-icons/bi";
 const Home: NextPage = () => {
   const searchDropdownItems = ["Vectors", "Photos", "PSD", "Videos"];
   return (
@@ -16,6 +17,19 @@ const Home: NextPage = () => {
           dropdownItems={searchDropdownItems}
           className="w-[30rem]"
         />
+      </div>
+      <div className="w-9/12 flex justify-between">
+        <div className="flex gap-x-6">
+          <div>Latest</div>
+          <div>Popular</div>
+          <div>|</div>
+          <div>Premium</div>
+          <div>Free</div>
+        </div>
+        <div className="flex gap-x-4 text-lg">
+          <CgOptions />
+          <BiBookmarks />
+        </div>
       </div>
     </div>
   );

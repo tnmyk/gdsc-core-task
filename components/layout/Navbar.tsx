630018af730093520b54fcd21e2ab1868f967374
Tanmaykk <tanmaykachroo@gmail.com>
2022-08-07 00:42:42 +0530

    Social Icons added

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -3,6 +3,19 @@ import { useState } from "react";
 import Dropdown from "../Dropdown";
 import { IoMdArrowDropdown } from "react-icons/io";
 
+const SocialIcon = ({ icon }: { icon: string }) => {
+  return (
+    <Link href="/">
+      <a
+        className="flex items-center justify-center shadow-lg bg-white
+        w-9 h-9 rounded-full"
+      >
+        <img src={icon} width={"20px"} />
+      </a>
+    </Link>
+  );
+};
+
 const Navbar = () => {
   const [dropdownOpen, setDropdownOpen] = useState<boolean>(false);
   const moreItems = ["About", "Contact", "Privacy Policy"];
@@ -48,8 +61,10 @@ const Navbar = () => {
           />
         </div>
       </div>
-      <div>
+      <div className="flex items-center gap-x-4">
         <button className="px-4 py-2 rounded border">+ Submit</button>
+        <SocialIcon icon="https://upload.wikimedia.org/wikipedia/commons/7/7e/Gmail_icon_%282020%29.svg" />
+        <SocialIcon icon="https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg" />
       </div>
     </nav>
   );

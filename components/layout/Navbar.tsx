8fe19e173300f0ca45b2ec7df792f1fed8e4349c
Tanmaykk <tanmaykachroo@gmail.com>
2022-08-06 23:18:45 +0530

    Nav changes + logo

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -1,13 +1,27 @@
+import Link from "next/link";
+
 const Navbar = () => {
   return (
-    <nav className="flex justify-between items-center py-8 px-[10%]">
-      <div className="flex gap-x-6">
-        <div>logo</div>
-        <div>Vector</div>
-        <div>Photos</div>
-        <div>PSD</div>
-        <div>Video</div>
-        <div>More</div>
+    <nav className="flex justify-between py-8 px-[10%]">
+      <div className="flex items-center gap-x-6 text-sm font-medium text-gray-500">
+        <Link href="/">
+          <a>
+            <img src="/logo.svg" alt="" width="40px" />
+          </a>
+        </Link>
+        <Link href="/">
+          <a>Vector</a>
+        </Link>
+        <Link href="/">
+          <a>Photos</a>
+        </Link>
+        <Link href="/">
+          <a>PSD</a>
+        </Link>
+        <Link href="/">
+          <a>Video</a>
+        </Link>
+        <button className="text-gray-700">More</button>
       </div>
       <div>
         <button className="px-4 py-2 rounded border">+ Submit</button>

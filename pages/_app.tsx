4286fcf1bdfdd5ac2e01b63f1c31ba10aa6754a9
Tanmaykk <tanmaykachroo@gmail.com>
2022-08-07 23:00:59 +0530

    Minor

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/layout/Layout";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <Head>
+        <title>GDSC Core Task</title>
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );

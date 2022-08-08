2544527264a6b8e7f17a54a91c3832b52ed70ba4
Tanmaykk <tanmaykachroo@gmail.com>
2022-08-08 15:48:13 +0530

    Refactor

---
+++ components/home/galleryData.ts
@@ -0,0 +1,134 @@
+export const galleryData = [
+  {
+    title: "Wave illustration",
+    isPremium: true,
+    imageURL:
+      "https://www.freeiconspng.com/uploads/abstract-circle-wave-logo-png-image-11.png",
+  },
+  {
+    title: "Batman illustration",
+    isPremium: true,
+    imageURL:
+      "https://w7.pngwing.com/pngs/509/584/png-transparent-batman-logo-sticker-batman-logo-comics-emblem-superhero.png",
+  },
+  {
+    title: "Med illustration",
+    isPremium: false,
+    imageURL:
+      "https://www.freeiconspng.com/uploads/vaccine-medical-icon-symbol-png-15.png",
+  },
+  {
+    title: "Dog illustration",
+    isPremium: false,
+    imageURL:
+      "https://www.freeiconspng.com/uploads/vaccine-medical-icon-symbol-png-15.png",
+  },
+  {
+    title: "Pen illustration",
+    isPremium: true,
+    imageURL:
+      "https://www.freeiconspng.com/uploads/abstract-circle-wave-logo-png-image-11.png",
+  },
+  {
+    title: "Cat illustration",
+    isPremium: true,
+    imageURL:
+      "https://w7.pngwing.com/pngs/509/584/png-transparent-batman-logo-sticker-batman-logo-comics-emblem-superhero.png",
+  },
+  {
+    title: "Pet illustration",
+    isPremium: true,
+    imageURL:
+      "https://www.freeiconspng.com/uploads/abstract-circle-wave-logo-png-image-11.png",
+  },
+  {
+    title: "Developer illustration",
+    isPremium: true,
+    imageURL:
+      "https://w7.pngwing.com/pngs/509/584/png-transparent-batman-logo-sticker-batman-logo-comics-emblem-superhero.png",
+  },
+  {
+    title: "Doctor illustration",
+    isPremium: false,
+    imageURL:
+      "https://www.freeiconspng.com/uploads/vaccine-medical-icon-symbol-png-15.png",
+  },
+  {
+    title: "Parent illustration",
+    isPremium: true,
+    imageURL:
+      "https://w7.pngwing.com/pngs/509/584/png-transparent-batman-logo-sticker-batman-logo-comics-emblem-superhero.png",
+  },
+  {
+    title: "Child illustration",
+    isPremium: true,
+    imageURL:
+      "https://www.freeiconspng.com/uploads/abstract-circle-wave-logo-png-image-11.png",
+  },
+  {
+    title: "Food illustration",
+    isPremium: false,
+    imageURL:
+      "https://www.freeiconspng.com/uploads/vaccine-medical-icon-symbol-png-15.png",
+  },
+  {
+    title: "Drink illustration",
+    isPremium: true,
+    imageURL:
+      "https://www.freeiconspng.com/uploads/abstract-circle-wave-logo-png-image-11.png",
+  },
+  {
+    title: "Wave illustration",
+    isPremium: true,
+    imageURL:
+      "https://www.freeiconspng.com/uploads/abstract-circle-wave-logo-png-image-11.png",
+  },
+  {
+    title: "Batman illustration",
+    isPremium: true,
+    imageURL:
+      "https://w7.pngwing.com/pngs/509/584/png-transparent-batman-logo-sticker-batman-logo-comics-emblem-superhero.png",
+  },
+  {
+    title: "Med illustration",
+    isPremium: false,
+    imageURL:
+      "https://www.freeiconspng.com/uploads/vaccine-medical-icon-symbol-png-15.png",
+  },
+  {
+    title: "Games illustration",
+    isPremium: true,
+    imageURL:
+      "https://w7.pngwing.com/pngs/509/584/png-transparent-batman-logo-sticker-batman-logo-comics-emblem-superhero.png",
+  },
+  {
+    title: "Clothing illustration",
+    isPremium: true,
+    imageURL:
+      "https://www.freeiconspng.com/uploads/abstract-circle-wave-logo-png-image-11.png",
+  },
+  {
+    title: "Tshirt illustration",
+    isPremium: false,
+    imageURL:
+      "https://www.freeiconspng.com/uploads/vaccine-medical-icon-symbol-png-15.png",
+  },
+  {
+    title: "Med illustration",
+    isPremium: false,
+    imageURL:
+      "https://www.freeiconspng.com/uploads/vaccine-medical-icon-symbol-png-15.png",
+  },
+  {
+    title: "Pic illustration",
+    isPremium: true,
+    imageURL:
+      "https://w7.pngwing.com/pngs/509/584/png-transparent-batman-logo-sticker-batman-logo-comics-emblem-superhero.png",
+  },
+];
+
+export const collectionsImagesData = [
+  `https://picsum.photos/seed/${Math.random()}/100`,
+  `https://picsum.photos/seed/${Math.random()}/100`,
+  `https://picsum.photos/seed/${Math.random()}/100`,
+];
